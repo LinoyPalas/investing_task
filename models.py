d36b1e8670f70502bd4d31acf8f56c4104212e73
Linoy Palas <50657183+LinoyPalas@users.noreply.github.com>
2023-02-04 16:04:19 +0200

    add LineItem mode
    LineItem model to store information about ad serving

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ class AdUnit(models.Model):
     """
     AdUnit model to store information about ad containers
     """
-    country = models.CharField(max_length=3)
+    country = models.CharField(max_length=3)  # ISO-3166 max char num is 3
     language = models.CharField(max_length=100)
     device = models.CharField(max_length=100)
     os = models.CharField(max_length=100)
@@ -15,3 +15,19 @@ class AdUnit(models.Model):
 
     def __str__(self):
         return f"{self.country}-{self.language}-{self.device}-{self.os}-{self.browser}"
+
+
+class LineItem(models.Model):
+    """
+    LineItem model to store information about ad serving
+    """
+    max_impressions = models.PositiveIntegerField()
+    rpm = models.FloatField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    ad_unit = models.ForeignKey(AdUnit, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.max_impressions}-{self.rpm}-{self.start_time}-{self.end_time}"
