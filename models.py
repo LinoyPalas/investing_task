8175506e8355d7c20b8a11b827ec210eadb83263
Linoy Palas <50657183+LinoyPalas@users.noreply.github.com>
2023-02-04 16:12:14 +0200

    Creative model to store information about ad content
    Creative model to store information about ad content

--- models.py
+++ models.py
@@ -35,3 +35,28 @@ class LineItem(models.Model):
 
     def __str__(self):
         return f"{self.max_impressions}-{self.rpm}-{self.start_time}-{self.end_time}"
+
+
+class Creative(models.Model):
+    """
+    Creative model to store information about ad content
+    """
+
+    CREATIVE_TYPE_CHOICES = [
+        ('image', 'image'),
+        ('html5', 'html5'),
+        ('native', 'native')
+    ]
+
+    creative_type = models.CharField(max_length=100, choices=CREATIVE_TYPE_CHOICES)
+    content = models.BinaryField()
+
+    # how to represent the following in pixel - pixels, defined as width x height (300x100
+    size = models.CharField(max_length=100)
+
+    line_item = models.ForeignKey(LineItem, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.creative_type}-{self.size}"
